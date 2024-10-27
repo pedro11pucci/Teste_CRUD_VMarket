e56fd317628af406c21bcf36bda80030ad8d21e2
pedro11pucci <pedro11pucci@gmail.com>
2024-10-27 20:46:18 -0300

    group routes

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,20 @@ use App\Http\Controllers\SupplierController;
 
 Route::view('/', 'home');
 
-Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers');
-Route::get('/suppliers/create', [SupplierController::class,'create'])->name('suppliers/create');
-Route::post('/suppliers/store', [SupplierController::class, 'store'])->name('suppliers/store');
-Route::get('/suppliers/{id}/edit', [SupplierController::class, 'edit'])->name('suppliers/edit');
-Route::put('/suppliers/{id}', [SupplierController::class,'update'])->name('suppliers/update');
-Route::delete('/suppliers/delete', [SupplierController::class, 'destroy'])->name('suppliers/delete');
+Route::prefix('suppliers')->group(function () {
+    Route::get('/', [SupplierController::class, 'index'])->name('suppliers');
+    Route::get('/create', [SupplierController::class, 'create'])->name('suppliers/create');
+    Route::post('/store', [SupplierController::class, 'store'])->name('suppliers/store');
+    Route::get('/{id}/edit', [SupplierController::class, 'edit'])->name('suppliers/edit');
+    Route::put('/{id}', [SupplierController::class, 'update'])->name('suppliers/update');
+    Route::delete('/delete', [SupplierController::class, 'destroy'])->name('suppliers/delete');
+});
 
-Route::get('/products', [ProductController::class, 'index'])->name('products');
-Route::get('/products/create', [ProductController::class,'create'])->name('products/create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products/store');
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products/edit');
-Route::put('/products/{id}', [ProductController::class,'update'])->name('products/update');
-Route::delete('/products/delete', [ProductController::class, 'destroy'])->name('products/delete');
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('products');
+    Route::get('/create', [ProductController::class, 'create'])->name('products/create');
+    Route::post('/store', [ProductController::class, 'store'])->name('products/store');
+    Route::get('/{id}/edit', [ProductController::class, 'edit'])->name('products/edit');
+    Route::put('/{id}', [ProductController::class, 'update'])->name('products/update');
+    Route::delete('/delete', [ProductController::class, 'destroy'])->name('products/delete');
+});
